6d70ccb38deb177bc6645fa588412a46f3f5cb6c
sushmagowda3455 <59113415+sushmagowda3455@users.noreply.github.com>
2020-01-31 11:07:56 +0530

    first commit

---
+++ mytest.java
@@ -0,0 +1,15 @@
+package newtest1;
+
+import org.testng.annotations.Test;
+
+public class mytest {
+
+
+	@Test
+	
+	private static void customertest() {
+		System.out.println("verified");
+		
+	}
+
+}
